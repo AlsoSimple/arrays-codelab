31ad3c1c8f02a3ef238d4d7e1d4c5e8930f8d1e6
AlsoSimple <daniellinde678@gmail.com>
2025-08-25 13:38:56 +0200

    Grøn, Gul og rød opgaver gennemført

--- assets/js/opgaver/arrayFunctions.js
+++ assets/js/opgaver/arrayFunctions.js
@@ -9,11 +9,13 @@ let myPartArray = ['partElement1', 'partElement2', 'PartElement3'];
 // opgave 1 tilføj element4 til enden myArray
 
 // din kode her...
+myArray.push('element4');
 console.log(myArray);
 
 // opgave 1 fjern element4 igen
 
 // din kode her...
+myArray.pop();
 console.log(myArray);
 
 //.........................................................
@@ -21,11 +23,13 @@ console.log(myArray);
 // opgave 2 tilføj element4 til starten myArray
 
 // din kode her...
+myArray.splice(0, 0, 'element4');
 console.log(myArray);
 
 // opgave 2 fjern element4 igen
 
 // din kode her...
+myArray.splice(0, 1);
 console.log(myArray);
 
 //.........................................................
@@ -39,7 +43,7 @@ let myVegetables = ['salat', 'blomkål', 'spidskål', 'agurk'];
  */
 
 // din kode her...
-
+myFruits.splice(2, 0, 'mandarin', 'fersken');
 
 
 console.log(myFruits);
@@ -47,7 +51,7 @@ console.log(myFruits);
 // opgave 3 fjern mandarin og fersken igen
 
 // din kode her...
-
+myFruits.splice(2, 2);
 
 console.log(myFruits);
 
@@ -58,7 +62,8 @@ træk blomkål og spidskål ud af arrayen myVegetables
 og ind i en ny array der hedder myCabbage*/
 
 // din kode her...
-
+myVegetables.slice(1, 3);
+let myCabbage = myVegetables.slice(1, 3);
 
 
 console.log(myCabbage);
@@ -70,7 +75,7 @@ console.log(myCabbage);
 
 
 // din kode her...
-
+let myYummies = myFruits.concat(myVegetables);
 
 
 console.log(myYummies);
@@ -84,7 +89,7 @@ console.log(myYummies);
 
 // din kode her...
 
-
+myYummies.sort();
 
 console.log(myYummies);
 
@@ -94,7 +99,7 @@ console.log(myYummies);
 
 
 // din kode her...
-
+myYummies.reverse();
 console.log(myYummies);
 
 //.........................................................
@@ -107,8 +112,11 @@ console.log(myYummies);
 let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // din kode her...
+let myNextNumbers = myNumbers.map(number => number + 7);
 console.log(myNextNumbers);
+let myLastNumbers = myNextNumbers.map(number => number * 12);
 console.log(myLastNumbers);
+
 
 //.........................................................
 /* opgave 8 filter()
@@ -116,6 +124,9 @@ skriv en kode der kan give os en ny array, udfra myNames, hvor navnet keld ikke 
 */
 let myNames = ['keld', 'søren', 'jens', 'tine', 'keld', 'ib', 'lene', 'keld', 'frederikke', 'line', 'kurt'];
 
+// din kode her...
+myNames = myNames.filter(name => name !== 'keld');
+console.log(myNames);
 
 //.........................................................
 
--- assets/js/opgaver/arrays.js
+++ assets/js/opgaver/arrays.js
@@ -1,9 +1,9 @@
 /* arrays */
 
 //Opgave 1
-const myArray =/*din array her*/;
+const myArray = ['element1', 'element2', 'element3', 'element4', 'element5'];
 
-
+console.log(myArray[2], myArray[4], myArray[0], myArray[3], myArray[1]);
 
 
 // opgave 2 console.logs
